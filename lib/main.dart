80883079d5d3ef4646ce96ea95098f1b7644f41a
unknown <mohamadbastin@gmail.com>
2021-08-30 12:01:11 +0430

    middle card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,92 +19,227 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.black,
+          child: Stack(
+            // fit: StackFit.expand,
+            alignment: AlignmentDirectional.center,
+            children: [
+              const GridTableThingy(
+                debug: true,
+                rows: 7,
+                columns: 12,
+                children: [
+                  BubbleItem(
+                    row: 3,
+                    column: 1,
+                    child: SizedBox(
+                      // height: 20,
+                      child: Text(
+                        "mamad",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  BubbleItem(
+                    row: 2,
+                    column: 10,
+                    child: FlareItem(child: Icon(Icons.airplanemode_on)),
+                  ),
+                  BubbleItem(
+                    row: 5,
+                    column: 0,
+                    child: FlareItem(child: Icon(Icons.media_bluetooth_on)),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 7 * 5,
+                width: MediaQuery.of(context).size.width / 12 * 8,
+                // height: 30,
+                child: Card(
+                    elevation: 10,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text("data"),
+                    )),
+              )
+            ],
+          )),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class GridTableThingy extends StatefulWidget {
+  const GridTableThingy(
+      {Key? key,
+      required this.rows,
+      required this.columns,
+      this.debug = false,
+      required this.children})
+      : super(key: key);
+
+  final int rows;
+  final int columns;
+  final bool debug;
+  final List<BubbleItem> children;
+
+  @override
+  _GridTableThingyState createState() => _GridTableThingyState();
+}
+
+class _GridTableThingyState extends State<GridTableThingy> {
+  List colors = [
+    Colors.red,
+    Colors.green,
+    // Colors.yellow,
+    Colors.blue,
+    // Colors.cyan,
+    Colors.orange,
+    Colors.grey,
+    Colors.lime,
+    Colors.pink,
+    Colors.purple,
+    // Colors.teal,
+    Colors.white,
+    // Colors.transparent
+  ];
+  Random random = Random();
+
+  int randomColor() {
+    return random.nextInt(colors.length);
+  }
+
+  List<double> _getCellSize() {
+    var width = (MediaQuery.of(context).size.width) / widget.columns;
+    var height = (MediaQuery.of(context).size.height) / widget.rows;
+
+    var ls = [height, width];
+    // print(ls);
+    return ls;
+  }
+
+  Widget _getChild(row, col) {
+    for (BubbleItem i in widget.children) {
+      // print(i.row);
+      if (i.row == row && i.column == col) {
+        return i.child;
+      }
+    }
+    if (widget.debug) {
+      return Text("$row  $col");
+    }
+    return const Text("");
+  }
+
+  List<Widget> _createCells(height, width) {
+    List<Widget> cells = [];
+    for (int i = 0; i < widget.rows; i++) {
+      for (int j = 0; j < widget.columns; j++) {
+        cells.add(SizedBox(
+          height: height,
+          width: width,
+          child: Container(
+            color: widget.debug ? colors[randomColor()] : Colors.transparent,
+            child: Center(child: _getChild(i, j)),
+          ),
+        ));
+      }
+    }
+
+    return cells;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    var size = _getCellSize();
+    var cellHeight = size[0];
+    var cellWidth = size[1];
+    return GridView.count(
+      crossAxisCount: widget.columns,
+      childAspectRatio: cellWidth / cellHeight,
+      children: _createCells(cellHeight, cellWidth),
+    );
+  }
+}
+
+class BubbleItem extends StatefulWidget {
+  const BubbleItem(
+      {Key? key, required this.row, required this.column, required this.child})
+      : super(key: key);
+
+  final int row;
+  final int column;
+  final Widget child;
+
+  @override
+  _BubbleItemState createState() => _BubbleItemState();
+}
+
+class _BubbleItemState extends State<BubbleItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class FlareItem extends StatefulWidget {
+  const FlareItem({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  _FlareItemState createState() => _FlareItemState();
+}
+
+class _FlareItemState extends State<FlareItem> {
+  List flares = [
+    "animations/blue.flr",
+    "animations/green.flr",
+    "animations/grey.flr",
+    "animations/red.flr",
+    "animations/yellow.flr"
+  ];
+
+  Random random = Random();
+
+  int randomFlare() {
+    return random.nextInt(flares.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.loose,
+      alignment: AlignmentDirectional.center,
+      // clipBehavior: ,
+      children: [
+        FlareActor(
+          flares[randomFlare()],
+          animation: "Alarm",
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        widget.child
+      ],
     );
   }
 }
