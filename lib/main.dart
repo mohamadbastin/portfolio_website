9b2a925ba7ef23355bc7c91baf37f53b19db424b
unknown <mohamadbastin@gmail.com>
2021-11-29 23:54:28 +0330

    complete introduction card design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
 import 'dart:ui';
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 import 'styles.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
@@ -75,7 +71,7 @@ class TopCard extends StatelessWidget {
             filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
             child: Card(
                 elevation: 10,
-                color: greyMain.withOpacity(0.7),
+                color: greyTopCard.withOpacity(0.7),
                 shape: RoundedRectangleBorder(
                     side: const BorderSide(width: 4, color: Colors.white),
                     borderRadius: BorderRadius.circular(42)),
@@ -90,7 +86,7 @@ class TopCard extends StatelessWidget {
                         width: constraints2.maxWidth * 0.38,
                         child: Container(
                           color: Colors.transparent,
-                          child: IntroductionCard(),
+                          child: const IntroductionCard(),
                         ),
                       ),
                       SizedBox(
@@ -117,85 +113,121 @@ class IntroductionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-      return Padding(
-        padding: EdgeInsets.only(
-          top: constraints.maxHeight * 0.072,
-          left: constraints.maxWidth * 0.112,
-          // right: constraints.maxWidth * 0.05
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SvgPicture.asset(
-              'assets/logo.svg',
-              height: constraints.maxHeight * 0.058,
-              width: constraints.maxWidth * 0.098,
-              color: Colors.white,
+      return Container(
+        decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              radius: 3.5,
+              center: Alignment(-0.2, -0.4),
+              colors: [Color(0xff1D1E21), Color(0xff313438)],
             ),
-            Padding(
-              padding: EdgeInsets.only(top: constraints.maxHeight * 0.217),
-              child: Text("Hi I'm",
-                  style: roboto(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white)),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: constraints.maxHeight * 0.039),
-              child: Text("Mohamad Bastin",
-                  style: roboto(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white)),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: constraints.maxHeight * 0.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  DefaultTextStyle(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(42), bottomLeft: Radius.circular(42))),
+        width: constraints.maxWidth,
+        height: constraints.maxHeight,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: constraints.maxHeight * 0.072,
+            left: constraints.maxWidth * 0.112,
+            // right: constraints.maxWidth * 0.05
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SvgPicture.asset(
+                'assets/logo.svg',
+                height: constraints.maxHeight * 0.058,
+                width: constraints.maxWidth * 0.098,
+                color: Colors.white,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: constraints.maxHeight * 0.217),
+                child: Text("Hi I'm",
                     style: roboto(
-                        fontSize: 36,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white),
-                    child: SizedBox(
-                      height: 150,
-                      child: Center(
-                        child: AnimatedTextKit(
-                          repeatForever: true,
-                          animatedTexts: [
-                            RotateAnimatedText('Flutter Developer',
-                                rotateOut: false,
-                                duration: const Duration(milliseconds: 600)),
-                            RotateAnimatedText('Backend Developer',
-                                rotateOut: false,
-                                duration: const Duration(milliseconds: 600)),
-                            RotateAnimatedText('Frontend Developer',
-                                rotateOut: false,
-                                duration: const Duration(milliseconds: 600)),
-                          ],
+                        fontSize: 48,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: constraints.maxHeight * 0.039),
+                child: Text("Mohamad Bastin",
+                    style: roboto(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: constraints.maxHeight * 0.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    DefaultTextStyle(
+                      style: roboto(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white),
+                      child: SizedBox(
+                        height: 150,
+                        child: Center(
+                          child: AnimatedTextKit(
+                            repeatForever: true,
+                            animatedTexts: [
+                              RotateAnimatedText('Flutter Developer',
+                                  rotateOut: false,
+                                  duration: const Duration(milliseconds: 600)),
+                              RotateAnimatedText('Backend Developer',
+                                  rotateOut: false,
+                                  duration: const Duration(milliseconds: 600)),
+                              RotateAnimatedText('Frontend Developer',
+                                  rotateOut: false,
+                                  duration: const Duration(milliseconds: 600)),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  // const SizedBox(width: 10.0),
-                  // SizedBox(
-                  //   height: 150,
-                  //   child: Center(
-                  //     child: Text(
-                  //       'Developer',
-                  //       style: roboto(
-                  //           fontSize: 36,
-                  //           fontWeight: FontWeight.w500,
-                  //           color: Colors.white),
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+                  ],
+                ),
               ),
+              Padding(
+                padding: EdgeInsets.only(top: constraints.minHeight * 0.024),
+                child: const ContactButton(),
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
+}
+
+class ContactButton extends StatelessWidget {
+  const ContactButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return SizedBox(
+        height: constraints.maxWidth * 0.17,
+        width: constraints.maxWidth * 0.693,
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: const RadialGradient(
+                radius: 2.5,
+                center: Alignment(-0.4, 0),
+                colors: [Color(0xffFFAEA3), Color(0xff59E1FF)],
+              ),
+              borderRadius: BorderRadius.circular(28)),
+          child: Center(
+            child: Text(
+              "Contact Me",
+              style: roboto(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 36,
+                  color: const Color(0xff27292C)),
             ),
-          ],
+          ),
         ),
       );
     });
--- lib/styles.dart
+++ lib/styles.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const greyMain = Color(0xff27292C);
-const greyTopCard = Color(0xff313438);
+const greyTopCard = Color(0xff202022);
 
 const roboto = GoogleFonts.roboto;
