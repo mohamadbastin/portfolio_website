2ab98dfa9ae528924c5ba2e625f620236971a891
unknown <mohamadbastin@gmail.com>
2021-11-29 22:17:26 +0330

    title animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:math';
 import 'dart:ui';
-
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'styles.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -78,10 +79,123 @@ class TopCard extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     side: const BorderSide(width: 4, color: Colors.white),
                     borderRadius: BorderRadius.circular(42)),
-                child: const Center(
-                  child: Text("data"),
-                )),
+                child: LayoutBuilder(builder:
+                    (BuildContext context2, BoxConstraints constraints2) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: double.maxFinite,
+                        width: constraints2.maxWidth * 0.38,
+                        child: Container(
+                          color: Colors.transparent,
+                          child: IntroductionCard(),
+                        ),
+                      ),
+                      SizedBox(
+                        height: double.maxFinite,
+                        width: constraints2.maxWidth * 0.62,
+                        child: Container(
+                          color: Colors.transparent,
+                        ),
+                      )
+                    ],
+                  );
+                })),
           ),
+        ),
+      );
+    });
+  }
+}
+
+class IntroductionCard extends StatelessWidget {
+  const IntroductionCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return Padding(
+        padding: EdgeInsets.only(
+          top: constraints.maxHeight * 0.072,
+          left: constraints.maxWidth * 0.112,
+          // right: constraints.maxWidth * 0.05
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              'assets/logo.svg',
+              height: constraints.maxHeight * 0.058,
+              width: constraints.maxWidth * 0.098,
+              color: Colors.white,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: constraints.maxHeight * 0.217),
+              child: Text("Hi I'm",
+                  style: roboto(
+                      fontSize: 48,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white)),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: constraints.maxHeight * 0.039),
+              child: Text("Mohamad Bastin",
+                  style: roboto(
+                      fontSize: 48,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white)),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: constraints.maxHeight * 0.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  DefaultTextStyle(
+                    style: roboto(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                    child: SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: AnimatedTextKit(
+                          repeatForever: true,
+                          animatedTexts: [
+                            RotateAnimatedText('Flutter Developer',
+                                rotateOut: false,
+                                duration: const Duration(milliseconds: 600)),
+                            RotateAnimatedText('Backend Developer',
+                                rotateOut: false,
+                                duration: const Duration(milliseconds: 600)),
+                            RotateAnimatedText('Frontend Developer',
+                                rotateOut: false,
+                                duration: const Duration(milliseconds: 600)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  // const SizedBox(width: 10.0),
+                  // SizedBox(
+                  //   height: 150,
+                  //   child: Center(
+                  //     child: Text(
+                  //       'Developer',
+                  //       style: roboto(
+                  //           fontSize: 36,
+                  //           fontWeight: FontWeight.w500,
+                  //           color: Colors.white),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
     });
--- lib/styles.dart
+++ lib/styles.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const greyMain = Color(0xff27292C);
 const greyTopCard = Color(0xff313438);
+
+const roboto = GoogleFonts.roboto;
